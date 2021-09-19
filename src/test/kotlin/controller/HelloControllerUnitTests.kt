8691ba18021380fc8f45842b506e88386dda2904
fjlopez <fjlopez@unizar.es>
2021-09-19 15:48:30 +0200

    Better format

--- src/test/kotlin/controller/HelloControllerUnitTests.kt
+++ src/test/kotlin/controller/HelloControllerUnitTests.kt
@@ -4,39 +4,38 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
-
 /*
-    This class contains a test that checks if the 'welcome' method
-    for class HelloController in file 'HelloController.kt' is 
-    executed successfully.
-*/
+ *    This class contains a test that checks if the 'welcome' method
+ *    for class HelloController in file 'HelloController.kt' is
+ *    executed successfully.
+ */
 class HelloControllerUnitTests {
     /*
-        Variable 'controller' that will allocate an instance of the
-        HelloController class. 'lateinit' implies it must be
-        initiallized later.
-    */
+     *   Variable 'controller' that will allocate an instance of the
+     *   HelloController class. 'lateinit' implies it must be
+     *   initiallized later.
+     */
     private lateinit var controller: HelloController
 
     /*
-        This function is used to initiallize the controller variable 
-        with its constructor. Cointains @BeforeEach tag since it
-        must be executed before the test.
-    */
+     *   This function is used to initiallize the controller variable
+     *   with its constructor. Cointains @BeforeEach tag since it
+     *   must be executed before the test.
+     */
     @BeforeEach
     fun setup() {
         controller = HelloController()
     }
 
     /*
-        Test that checks if the 'welcome' method is executed
-        successfully. 
-    */
+     *   Test that checks if the 'welcome' method is executed
+     *   successfully.
+     */
     @Test
     fun testMessage() {
-        //Map used for 'welcome' controller method
-        val map = mutableMapOf<String,Any>()    
-        
+        // Map used for 'welcome' controller method
+        val map = mutableMapOf<String, Any>()
+
         // Executes 'welcome' controller method and saves returned
         // value in variable 'view'
         val view = controller.welcome(map)
